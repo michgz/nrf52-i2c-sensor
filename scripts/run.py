6ef8793bc1fe79422e781d98170985ef8773264a
michgz <mdr.nz11@gmail.com>
2022-12-30 09:30:47 +0000

    Set up the file structure

---
+++ scripts/run.py
@@ -0,0 +1,21 @@
+"""
+Reads and records buffered data from a device
+"""
+
+import datetime
+import textwrap
+import pathlib
+import struct
+import asyncio
+
+from bleak import BleakClient
+
+
+
+def run(ADDR):
+    pass
+
+
+if __name__=="__main__":
+    run("F2:9D:C9:B8:79:9D")
+
