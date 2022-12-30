8bbb1dd4d4a97d83b161819b3276863457ec2b6d
michgz <mdr.nz11@gmail.com>
2022-12-30 09:45:29 +0000

    Post-process all

--- scripts/run.py
+++ scripts/run.py
@@ -6,6 +6,7 @@ import datetime
 import textwrap
 import pathlib
 import struct
+import random
 import asyncio
 
 from bleak import BleakClient
@@ -13,7 +14,85 @@ from bleak import BleakClient
 
 
 def run(ADDR):
-    pass
+  
+  
+    RXED = b""
+    TS = None
+    
+    def notification(A, B):
+        global RXED
+        print(" >  " + str(B))
+        RXED += B
+        
+    async def main(ADDR):
+        global RXED
+        global TS
+        
+        async with BleakClient(ADDR, timeout=30.) as client:
+            for service in client.services:
+                print(service)
+                print(service.description)
+                print(service.uuid)
+                if service.description.startswith("Nordic"):
+                    for char in service.characteristics:
+                        print(char)
+                        print(char.description)
+                        print(char.uuid)
+                        print(char.service_handle)
+                        print(char.properties)
+                        print(char.handle)
+                        
+                        if str(char.uuid) == "6e400003-b5a3-f393-e0a9-e50e24dcca9e":    # Nordic UART service, read characteristic
+                            TS = datetime.datetime.now()
+                            
+                            
+                            # Turn on notifications, and wait for 10 seconds. The device
+                            # should push any data it has during that time
+                            
+                            x = await client.start_notify(char, notification)
+                            print(x)
+                            
+                            await asyncio.sleep(10.)
+                            
+                            x = await client.stop_notify(char, notification)
+                            print(x)
+                            
+
+    RXED = b""
+    TS = None
+    asyncio.run(main(ADDR))
+    
+    if TS is not None:
+        with open(pathlib.Path(__file__).parent.joinpath("Received_Data_{0}_{1:07d}.txt".format(  \
+                    TS.strftime("%y%m%d%H%M%S"), \
+                    random.randint(0, 9999999)   \
+                ), "w") as f1:
+            f1.write("Timestamp: {0}\n".format(TS.strftime("%d/%m/%Y %H:%M:%S")))
+            f1.write("Data:\n")
+            f1.write(textwrap.indent(textwrap.fill(RXED.hex(" ").upper(), 48), "    ") + "\n\n")
+            f1.write("Decode: (times are approximate)\n")
+            
+            TIMES = []
+            TEMPS = []
+            HUMS = []
+            
+            I = 0
+            while I+8 <= len(RXED):
+                TIMES.append(struct.unpack_from("<I", RXED, I+0)[0])
+                TEMPS.append(struct.unpack_from(">H", RXED, I+4)[0])
+                HUMS.append(struct.unpack_from(">H", RXED, I+6)[0])
+                
+                I += 8
+            
+            if len(TIMES) > 0:
+                START_TIME = TS - datetime.timedelta(seconds = max(TIMES)) # Assumes the last time is time of reading. That could be off quite a bit
+                
+            for N in range(len(TIMES)):
+                f1.write("{0}".format((START_TIME + datetime.timedelta(seconds = TIMES[N])).strftime("%d/%m/%Y %H:%M:%S").ljust(26)))
+                f1.write("{0:0.2f} degC".format( -45. + 175.*(float(TEMPS[N])/65535.)   ).ljust(24))
+                f1.write("{0:0.1f} %RH".format( -6. + 125.*(float(HUMS[N])/65535.)   ).ljust(24) +  "\n")
+
+
 
 
 if __name__=="__main__":
